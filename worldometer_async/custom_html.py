eeaf6856b0fd61a8669fd3110f6b06f3c3b53489
HoPHNiDev <hophniagent@gmail.com>
2025-02-11 01:03:36 +0500

    init

---
+++ worldometer_async/custom_html.py
@@ -0,0 +1,94 @@
+from requests_html import (
+    HTML,
+    DEFAULT_ENCODING,
+    DEFAULT_URL,
+    MaxRetries,
+    HTMLResponse,
+    BaseSession,
+    HTMLSession,
+    AsyncHTMLSession,
+)
+from typing import Union
+
+
+class HTML(HTML):
+    async def arender(
+        self,
+        retries: int = 8,
+        script: str = None,
+        wait: float = 0.2,
+        scrolldown=False,
+        sleep: int = 0,
+        reload: bool = True,
+        timeout: Union[float, int] = 8.0,
+        keep_page: bool = False,
+    ):
+        """Async version of render. Takes same parameters."""
+
+        self.browser = await self.session.browser
+        content = None
+
+        # Automatically set Reload to False, if example URL is being used.
+        if self.url == DEFAULT_URL:
+            reload = False
+
+        for _ in range(retries):
+            if not content:
+                try:
+
+                    content, result, page = await self._async_render(
+                        url=self.url,
+                        script=script,
+                        sleep=sleep,
+                        wait=wait,
+                        content=self.html,
+                        reload=reload,
+                        scrolldown=scrolldown,
+                        timeout=timeout,
+                        keep_page=keep_page,
+                    )
+                except TypeError:
+                    pass
+            else:
+                break
+
+        if not content:
+            raise MaxRetries("Unable to render the page. Try increasing timeout")
+
+        html = HTML(
+            url=self.url,
+            html=content.encode(DEFAULT_ENCODING),
+            session=self.session,
+            default_encoding=DEFAULT_ENCODING,
+        )
+        self.__dict__.update(html.__dict__)
+        self.page = page
+        return result
+
+
+class HTMLResponse(HTMLResponse):
+    @property
+    def html(self) -> HTML:
+        if not self._html:
+            self._html = HTML(
+                session=self.session,
+                url=self.url,
+                html=self.content,
+                default_encoding=self.encoding,
+            )
+
+        return self._html
+
+
+class BaseSession(BaseSession):
+    def response_hook(self, response, **kwargs) -> HTMLResponse:
+        """Change response enconding and replace it by a HTMLResponse."""
+        if not response.encoding:
+            response.encoding = DEFAULT_ENCODING
+        return HTMLResponse._from_response(response, self)
+
+
+class HTMLSession(HTMLSession, BaseSession): ...
+
+
+class AsyncHTMLSession(AsyncHTMLSession, BaseSession): ...
